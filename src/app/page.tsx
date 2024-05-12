cbd6fee4250329a108c64537351dbe8238159dd4
Bakate <b.ba@startupflow.io>
2024-05-12 15:56:25 +0200

    init the project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-1 gap-4 place-items-center">
+      <h1 className="text-2xl text-center">hello and welcome</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+}
